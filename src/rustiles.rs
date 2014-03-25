425cfaae0d70ebfff3d2a8f8bc956245f765b3ed
Alex Morega <alex@grep.ro>
2014-03-25 23:52:48 +0200

    trivial http server

--- src/rustiles.rs
+++ src/rustiles.rs
@@ -1,8 +1,44 @@
+extern crate http;
+
+use std::vec::Vec;
+use std::io::net::ip::{SocketAddr, Ipv4Addr};
+use std::io::Writer;
+use http::server::{Config, Server, Request, ResponseWriter};
+use http::headers;
+
+
 #[test]
 fn test_nothing() {
     assert_eq!(1, 1);
 }
 
 
+#[deriving(Clone)]
+struct TileServer;
+
+
+impl Server for TileServer {
+    fn get_config(&self) -> Config {
+        Config {
+            bind_address: SocketAddr {
+                ip: Ipv4Addr(0, 0, 0, 0),
+                port: 8001,
+            },
+        }
+    }
+
+    fn handle_request(&self, _r: &Request, w: &mut ResponseWriter) {
+        w.headers.content_type = Some(headers::content_type::MediaType {
+            type_: ~"text",
+            subtype: ~"html",
+            parameters: Vec::new(),
+        });
+
+        w.write(bytes!("Hello world!\n")).unwrap();
+    }
+}
+
+
 fn main() {
+    TileServer.serve_forever();
 }
