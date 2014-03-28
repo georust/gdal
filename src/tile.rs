20bea1b7c90dab72289d8111f760bf85ecf6c171
Alex Morega <alex@grep.ro>
2014-03-28 09:03:15 +0200

    create in-memory tile

--- src/tile.rs
+++ src/tile.rs
@@ -10,12 +10,13 @@ mod gdal;
 fn main() {
     let memory_driver = gdal::get_driver("MEM").unwrap();
 
-    println!("hello tile! {}", memory_driver.get_short_name());
-
     let source = gdal::open(&Path::new(args()[1])).unwrap();
     println!(
         "size: {}, bands: {}",
         source.get_raster_size(),
         source.get_raster_count()
     );
+
+    let tile = memory_driver.create("", 256, 256, 3).unwrap();
+    println!("tile size: {}", tile.get_raster_size());
 }
