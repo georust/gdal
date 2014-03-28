575edb97ebae517f03911b0661c98a19c8cd74d4
Alex Morega <alex@grep.ro>
2014-03-28 11:18:15 +0200

    take a tile from the raster image

--- src/tile.rs
+++ src/tile.rs
@@ -2,6 +2,7 @@ extern crate sync;
 
 use std::os::args;
 use std::path::Path;
+use std::io::{File, TempDir, stdio};
 
 #[allow(dead_code)]
 mod gdal;
@@ -9,6 +10,7 @@ mod gdal;
 
 fn main() {
     let memory_driver = gdal::get_driver("MEM").unwrap();
+    let png_driver = gdal::get_driver("PNG").unwrap();
 
     let source = gdal::open(&Path::new(args()[1])).unwrap();
     println!(
@@ -18,5 +20,14 @@ fn main() {
     );
 
     let tile = memory_driver.create("", 256, 256, 3).unwrap();
-    println!("tile size: {}", tile.get_raster_size());
+    for band in range(1, 4) {
+        let raster = source.read_raster(band, 10000, 1600, 3000, 2000, 256, 256);
+        tile.write_raster(band, 0, 0, 256, 256, raster);
+    }
+
+    let tmp = TempDir::new("rustile").unwrap();
+    let tile_path = tmp.path().join("tile.png");
+    tile.create_copy(png_driver, tile_path.as_str().unwrap());
+    let tile_data = File::open(&tile_path).read_to_end().unwrap();
+    assert!(stdio::stdout_raw().write(tile_data).is_ok());
 }
