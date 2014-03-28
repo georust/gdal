b5e53ff872c1bd6426df764f827a82463ea8538f
Alex Morega <alex@grep.ro>
2014-03-28 09:02:11 +0200

    open source image

--- src/tile.rs
+++ src/tile.rs
@@ -1,5 +1,8 @@
 extern crate sync;
 
+use std::os::args;
+use std::path::Path;
+
 #[allow(dead_code)]
 mod gdal;
 
@@ -8,4 +11,11 @@ fn main() {
     let memory_driver = gdal::get_driver("MEM").unwrap();
 
     println!("hello tile! {}", memory_driver.get_short_name());
+
+    let source = gdal::open(&Path::new(args()[1])).unwrap();
+    println!(
+        "size: {}, bands: {}",
+        source.get_raster_size(),
+        source.get_raster_count()
+    );
 }
