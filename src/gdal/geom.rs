61004c68a7df2f2d387489de198750c2b8880ab7
Alex Morega <alex@grep.ro>
2014-03-29 18:30:40 +0200

    implement point scaling

--- src/gdal/geom.rs
+++ src/gdal/geom.rs
@@ -8,9 +8,17 @@ pub fn Point<T>(x: T, y: T) -> Point<T> {
     return Point{x: x, y: y};
 }
 
+
 impl<T:Clone + Add<T,T>> Add<Point<T>, Point<T>> for Point<T> {
     fn add(&self, other: &Point<T>) -> Point<T> {
         return Point(self.x + other.x, self.y + other.y);
+    }
+}
+
+
+impl<T:Clone + Mul<T,T>> Point<T> {
+    pub fn scale(&self, factor: T) -> Point<T> {
+        return Point(self.x * factor, self.y * factor);
     }
 }
 
@@ -21,4 +29,11 @@ fn test_add() {
     let p2 = Point(1, 5);
     let p3 = p1 + p2;
     assert_eq!((p3.x, p3.y), (3, 8));
+}
+
+
+#[test]
+fn test_scale() {
+    let p = Point(2, 3).scale(2);
+    assert_eq!((p.x, p.y), (4, 6));
 }
