0d73a909ce68bb5573530b758a827a534f743ab0
Alex Morega <alex@grep.ro>
2014-03-29 16:35:38 +0200

    calculate source window using Point2D and coords

--- src/tile.rs
+++ src/tile.rs
@@ -1,8 +1,10 @@
 extern crate sync;
+extern crate geom;
 
 use std::os::args;
 use std::path::Path;
 use std::io::{File, TempDir, stdio};
+use geom::point::Point2D;
 
 #[allow(dead_code)]
 mod gdal;
@@ -13,15 +15,38 @@ fn main() {
     let png_driver = gdal::driver::get_driver("PNG").unwrap();
 
     let source = gdal::dataset::open(&Path::new(args()[1])).unwrap();
+    let (width, height) = source.get_raster_size();
+    let source_bounds = Point2D(width as f64, height as f64);
     assert!(stdio::stderr().write(format!(
         "size: {}, bands: {}",
-        source.get_raster_size(),
+        (width, height),
         source.get_raster_count()
     ).as_bytes()).is_ok());
 
+    fn xy(lng_lat: &Point2D<f64>, source_bounds: &Point2D<f64>) -> Point2D<f64> {
+        let x = (lng_lat.x + 180.) / 360. * source_bounds.x;
+        let y = (90. - lng_lat.y) / 180. * source_bounds.y;
+        return Point2D(x, y);
+    }
+
     let tile = memory_driver.create("", 256, 256, 3).unwrap();
     for band in range(1, 4) {
-        let raster = source.read_raster(band, 10000, 1600, 3000, 2000, 256, 256);
+        let nw: Point2D<f64> = Point2D(-13., 64.);
+        let se: Point2D<f64> = Point2D(37., 30.);
+
+        let xy_min = xy(&nw, &source_bounds);
+        let xy_max = xy(&se, &source_bounds);
+        let xy_bounds = xy_max - xy_min;
+
+        let raster = source.read_raster(
+            band,
+            xy_min.x as int,
+            xy_min.y as int,
+            xy_bounds.x as uint,
+            xy_bounds.y as uint,
+            256,
+            256
+        );
         tile.write_raster(band, 0, 0, 256, 256, raster);
     }
 
