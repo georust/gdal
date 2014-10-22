f81739871851984353c36581bc2f8cdd60d3e631
Alex Morega <alex@grep.ro>
2014-10-22 15:50:04 +0300

    use to_string()

--- src/gdal/utils.rs
+++ src/gdal/utils.rs
@@ -4,5 +4,5 @@ use std::c_str::CString;
 
 pub fn _string(raw_ptr: *const c_char) -> String {
     let c_string = unsafe { CString::new(raw_ptr, false) };
-    return String::from_str(c_string.as_str().unwrap());
+    return c_string.as_str().unwrap().to_string();
 }
