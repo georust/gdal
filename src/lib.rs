3278cc69fe12ce3742bf1977a564980e471408dd
Alex Morega <alex@grep.ro>
2015-01-28 23:08:57 +0200

    move version_info to its own module

--- src/lib.rs
+++ src/lib.rs
@@ -5,53 +5,16 @@
 extern crate libc;
 #[cfg(test)] extern crate test;
 
-use libc::c_char;
-use std::ffi::CString;
+pub use version::version_info;
 
 mod utils;
+pub mod version;
 pub mod raster;
 pub mod vector;
 pub mod proj;
 pub mod geom;
 
-
-#[link(name="gdal")]
-extern {
-    fn GDALVersionInfo(key: *const c_char) -> *const c_char;
-}
-
-
-pub fn version_info(key: &str) -> String {
-    let c_key = CString::from_slice(key.as_bytes());
-    return utils::_string(unsafe { GDALVersionInfo(c_key.as_ptr()) });
-}
-
-
 #[derive(Clone, Copy, PartialEq, Show)]
 pub struct GdalError {
     pub desc: &'static str,
-}
-
-
-#[cfg(test)]
-mod tests {
-    use super::version_info;
-
-
-    #[test]
-    fn test_version_info() {
-        let release_date = version_info("RELEASE_DATE");
-        let release_name = version_info("RELEASE_NAME");
-        let version_text = version_info("--version");
-
-        let expected_text: String = format!(
-            "GDAL {}, released {}/{}/{}",
-            release_name,
-            release_date.as_slice().slice(0, 4),
-            release_date.as_slice().slice(4, 6),
-            release_date.as_slice().slice(6, 8),
-        );
-
-        assert_eq!(version_text, expected_text);
-    }
 }
