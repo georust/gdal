e52b87cb43d6ccefa066e13ddf72160079665e21
Alex Morega <alex@grep.ro>
2015-05-10 11:41:44 +0300

    conform to rfc 592
    change `c_str_to_bytes()` to `CStr::from_ptr().to_bytes()`

--- src/utils.rs
+++ src/utils.rs
@@ -1,9 +1,9 @@
 use libc::c_char;
-use std::ffi::c_str_to_bytes;
+use std::ffi::CStr;
 use std::str;
 
 
 pub fn _string(raw_ptr: *const c_char) -> String {
-    let bytes = unsafe { c_str_to_bytes(&raw_ptr) };
-    return str::from_utf8(bytes).unwrap().to_string();
+    let c_str = unsafe { CStr::from_ptr(raw_ptr) };
+    return str::from_utf8(c_str.to_bytes()).unwrap().to_string();
 }
