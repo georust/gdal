65f4a0bba6e610c5146a4e79bc4d4e3a621e32c0
Alex Morega <alex@grep.ro>
2015-07-12 16:05:38 +0300

    crate docs

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,22 @@
+//! [GDAL](http://gdal.org/) bindings for Rust.
+//!
+//! A high-level API to access the GDAL library, for vector and raster data.
+//!
+//! ## Use
+//!
+//! ```
+//! use std::path::Path;
+//! use gdal::vector::Dataset;
+//!
+//! let dataset = Dataset::open(Path::new("fixtures/roads.geojson")).unwrap();
+//! let layer = dataset.layer(0).unwrap();
+//! for feature in layer.features() {
+//!     let highway_field = feature.field("highway").unwrap();
+//!     let geometry = feature.geometry();
+//!     println!("{} {}", highway_field.as_string(), geometry.wkt());
+//! }
+//! ```
+
 #![crate_name="gdal"]
 #![crate_type="lib"]
 
