89632668bae1096d3eb7c32bb9f378ebae8fd308
mthh <mattgeoc@autistici.org>
2017-01-15 17:51:37 +0100

    fix method name in test in comment

--- src/vector/mod.rs
+++ src/vector/mod.rs
@@ -11,7 +11,7 @@
 //! for feature in layer.features() {
 //!     let highway_field = feature.field("highway").unwrap();
 //!     let geometry = feature.geometry();
-//!     println!("{} {}", highway_field.as_string(), geometry.wkt().unwrap());
+//!     println!("{} {}", highway_field.to_string(), geometry.wkt().unwrap());
 //! }
 //! ```
 
