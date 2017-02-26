f390f6400e0e770eec201d5de3fd633dbcaf4145
Johannes Drönner <droenner@informatik.uni-marburg.de>
2017-02-26 17:50:02 +0100

    add missing else block to build.rs. use the same version of libc in rust-gdal and gdal-sys.

--- gdal-sys/build.rs
+++ gdal-sys/build.rs
@@ -21,24 +21,25 @@ fn main() {
         if lib_search_path.exists() && lib_path.exists() {
             println!("cargo:rustc-link-search={}", lib_search_path.to_string_lossy());
             println!("cargo:rustc-link-lib={}={}", link_type, format!("{}{}",lib_name, lib_suffix));            
-        }
+        } else {
 
-        #[cfg(target_env="msvc")]
-        {
-            panic!("windows-msvc requires gdal_i.lib to be found in $GDAL_HOME\\lib.");
-        }
-
-        #[cfg(target_env="gnu")]
-        {
-            // detect if a gdal{version}.dll is available
-            let versions = [201, 200, 111, 110];
-            let bin_path = Path::new(&home_path).join("bin");
-            if let Some(version) = versions.iter().find(|v| bin_path.join(&format!("{}{}.dll", lib_name, v)).exists()){
-                println!("cargo:rustc-link-search={}", bin_path.to_string_lossy());
-                println!("cargo:rustc-link-lib={}={}", link_type, format!("{}{}",lib_name, version));
+            #[cfg(target_env="msvc")]
+            {
+                panic!("windows-msvc requires gdal_i.lib to be found in $GDAL_HOME\\lib.");
             }
-            else {
-                panic!("windows-gnu requires either gdal_i.lib in $GDAL_HOME\\lib OR gdal{version}.dll in $GDAL_HOME\\bin.");
+
+            #[cfg(target_env="gnu")]
+            {
+                // detect if a gdal{version}.dll is available
+                let versions = [201, 200, 111, 110];
+                let bin_path = Path::new(&home_path).join("bin");
+                if let Some(version) = versions.iter().find(|v| bin_path.join(&format!("{}{}.dll", lib_name, v)).exists()){
+                    println!("cargo:rustc-link-search={}", bin_path.to_string_lossy());
+                    println!("cargo:rustc-link-lib={}={}", link_type, format!("{}{}",lib_name, version));
+                }
+                else {
+                    panic!("windows-gnu requires either gdal_i.lib in $GDAL_HOME\\lib OR gdal{version}.dll in $GDAL_HOME\\bin.");
+                }
             }
         }             
     }
