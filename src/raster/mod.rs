a9d010e8ba86b730d62a0836cd9357ea05aa79c8
Austin Davis <austinvdavis@gmail.com>
2018-02-10 10:18:03 -0600

    made gdaltype public

--- src/raster/mod.rs
+++ src/raster/mod.rs
@@ -4,11 +4,12 @@ pub use raster::dataset::{Dataset, Buffer, ByteBuffer};
 pub use raster::driver::Driver;
 pub use raster::warp::reproject;
 pub use raster::rasterband::{RasterBand};
+pub use raster::types::GdalType;
 
 pub use gdal_sys::gdal_enums;
 pub use gdal_sys::cpl_error::{CPLErr};
 
-mod types;
+pub mod types;
 pub mod dataset;
 pub mod driver;
 pub mod warp;
