49747dbb8c9dc650ae38fb536429f67707d10b51
Johannes Drönner <droenner@informatik.uni-marburg.de>
2020-09-30 16:44:21 +0200

    use openex parameters

--- src/dataset.rs
+++ src/dataset.rs
@@ -1,4 +1,4 @@
-use std::{ffi::CString, path::Path, ptr, sync::Once};
+use std::{ffi::CString, ffi::NulError, path::Path, ptr, sync::Once};
 
 use crate::utils::{_last_cpl_err, _last_null_pointer_err, _string};
 use crate::{
@@ -16,6 +16,7 @@ use crate::errors::*;
 pub type GeoTransform = [c_double; 6];
 static START: Once = Once::new();
 
+#[derive(Debug)]
 pub struct Dataset {
     c_dataset: GDALDatasetH,
 }
@@ -44,22 +45,91 @@ impl Dataset {
     pub fn open_ex(
         path: &Path,
         open_flags: Option<GDALAccess::Type>,
-        _allowed_drivers: Option<&str>, // TODO: use parameters
-        _open_options: Option<&str>,
-        _sibling_files: Option<&str>,
+        allowed_drivers: Option<&[&str]>, // TODO: use parameters
+        open_options: Option<&[&str]>,
+        sibling_files: Option<&[&str]>,
     ) -> Result<Dataset> {
         _register_drivers();
         let filename = path.to_string_lossy();
         let c_filename = CString::new(filename.as_ref())?;
         let c_open_flags = open_flags.unwrap_or(GDALAccess::GA_ReadOnly); // This defaults to GdalAccess::GA_ReadOnly
 
+        // handle driver params:
+        // we need to keep the CStrings and the pointers around
+        let c_allowed_drivers = allowed_drivers.map(|d| {
+            d.into_iter()
+                .map(|&s| CString::new(s))
+                .collect::<std::result::Result<Vec<CString>, NulError>>()
+        });
+        let c_drivers_vec = match c_allowed_drivers {
+            Some(Err(e)) => return Err(e.into()),
+            Some(Ok(c_drivers_vec)) => c_drivers_vec,
+            None => Vec::from([]),
+        };
+        let mut c_drivers_ptrs = c_drivers_vec.iter().map(|s| s.as_ptr()).collect::<Vec<_>>();
+        c_drivers_ptrs.push(ptr::null());
+
+        let c_drivers_ptr = if allowed_drivers.is_some() {
+            c_drivers_ptrs.as_ptr()
+        } else {
+            ptr::null()
+        };
+
+        // handle open options params:
+        // we need to keep the CStrings and the pointers around
+        let c_open_options = open_options.map(|d| {
+            d.into_iter()
+                .map(|&s| CString::new(s))
+                .collect::<std::result::Result<Vec<CString>, NulError>>()
+        });
+        let c_open_options_vec = match c_open_options {
+            Some(Err(e)) => return Err(e.into()),
+            Some(Ok(c_open_options_vec)) => c_open_options_vec,
+            None => Vec::from([]),
+        };
+        let mut c_open_options_ptrs = c_open_options_vec
+            .iter()
+            .map(|s| s.as_ptr())
+            .collect::<Vec<_>>();
+        c_open_options_ptrs.push(ptr::null());
+
+        let c_open_options_ptr = if open_options.is_some() {
+            c_open_options_ptrs.as_ptr()
+        } else {
+            ptr::null()
+        };
+
+        // handle sibling files params:
+        // we need to keep the CStrings and the pointers around
+        let c_sibling_files = sibling_files.map(|d| {
+            d.into_iter()
+                .map(|&s| CString::new(s))
+                .collect::<std::result::Result<Vec<CString>, NulError>>()
+        });
+        let c_sibling_files_vec = match c_sibling_files {
+            Some(Err(e)) => return Err(e.into()),
+            Some(Ok(c_sibling_files_vec)) => c_sibling_files_vec,
+            None => Vec::from([]),
+        };
+        let mut c_sibling_files_ptrs = c_sibling_files_vec
+            .iter()
+            .map(|s| s.as_ptr())
+            .collect::<Vec<_>>();
+        c_sibling_files_ptrs.push(ptr::null());
+
+        let c_sibling_files_ptr = if sibling_files.is_some() {
+            c_sibling_files_ptrs.as_ptr()
+        } else {
+            ptr::null()
+        };
+
         let c_dataset = unsafe {
             gdal_sys::GDALOpenEx(
                 c_filename.as_ptr(),
                 c_open_flags,
-                ptr::null(),
-                ptr::null(),
-                ptr::null(),
+                c_drivers_ptr,
+                c_open_options_ptr,
+                c_sibling_files_ptr,
             )
         };
         if c_dataset.is_null() {
@@ -243,5 +313,94 @@ impl Drop for Dataset {
         unsafe {
             gdal_sys::GDALClose(self.c_dataset);
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    macro_rules! fixture {
+        ($name:expr) => {
+            Path::new(file!())
+                .parent()
+                .unwrap()
+                .parent()
+                .unwrap()
+                .join("fixtures")
+                .as_path()
+                .join($name)
+                .as_path()
+        };
+    }
+
+    #[test]
+    fn test_open_vector() {
+        Dataset::open(fixture!("roads.geojson")).unwrap();
+    }
+
+    #[test]
+    fn test_open_ex_ro_vector() {
+        Dataset::open_ex(
+            fixture!("roads.geojson"),
+            Some(GDALAccess::GA_ReadOnly),
+            None,
+            None,
+            None,
+        )
+        .unwrap();
+    }
+
+    #[test]
+    fn test_open_ex_update_vector() {
+        Dataset::open_ex(
+            fixture!("roads.geojson"),
+            Some(GDALAccess::GA_Update),
+            None,
+            None,
+            None,
+        )
+        .unwrap();
+    }
+
+    #[test]
+    fn test_open_ex_allowed_driver_vector() {
+        Dataset::open_ex(
+            fixture!("roads.geojson"),
+            None,
+            Some(&["GeoJSON"]),
+            None,
+            None,
+        )
+        .unwrap();
+    }
+
+    #[test]
+    fn test_open_ex_allowed_driver_vector_fail() {
+        Dataset::open_ex(fixture!("roads.geojson"), None, Some(&["TIFF"]), None, None).unwrap_err();
+    }
+
+    #[test]
+    fn test_open_ex_open_option() {
+        Dataset::open_ex(
+            fixture!("roads.geojson"),
+            None,
+            None,
+            Some(&["FLATTEN_NESTED_ATTRIBUTES=YES"]),
+            None,
+        )
+        .unwrap();
+    }
+
+    #[test]
+    fn test_layer_count() {
+        let ds = Dataset::open(fixture!("roads.geojson")).unwrap();
+        assert_eq!(ds.layer_count(), 1);
+    }
+
+    #[test]
+    fn test_raster_count_on_vector() {
+        let ds = Dataset::open(fixture!("roads.geojson")).unwrap();
+        assert_eq!(ds.raster_count(), 0);
     }
 }
