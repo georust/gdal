d8c3474007501ded1400bbcd0dc1e5376f59e566
José Padarian <spadarian@gmail.com>
2021-01-17 09:55:59 +1100

    Linked overview lifetime to Dataset.
    This is to ensure that the overview has the lifetime of the
    original dataset and not the lifetime of the Rasterband from
    which the overview is created.

--- src/raster/rasterband.rs
+++ src/raster/rasterband.rs
@@ -1,5 +1,3 @@
-use std::marker::PhantomData;
-
 use crate::dataset::Dataset;
 use crate::gdal_major_object::MajorObject;
 use crate::metadata::Metadata;
@@ -20,7 +18,7 @@ use crate::errors::*;
 /// from being dropped before the band.
 pub struct RasterBand<'a> {
     c_rasterband: GDALRasterBandH,
-    phantom: PhantomData<&'a Dataset>,
+    dataset: &'a Dataset,
 }
 
 impl<'a> RasterBand<'a> {
@@ -28,10 +26,10 @@ impl<'a> RasterBand<'a> {
     ///
     /// # Safety
     /// This method operates on a raw C pointer
-    pub unsafe fn from_c_rasterband(_: &'a Dataset, c_rasterband: GDALRasterBandH) -> Self {
+    pub unsafe fn from_c_rasterband(dataset: &'a Dataset, c_rasterband: GDALRasterBandH) -> Self {
         RasterBand {
             c_rasterband,
-            phantom: PhantomData,
+            dataset,
         }
     }
 
@@ -303,14 +301,14 @@ impl<'a> RasterBand<'a> {
         unsafe { Ok(gdal_sys::GDALGetOverviewCount(self.c_rasterband)) }
     }
 
-    pub fn overview(&self, dataset: &'a Dataset, overview_index: isize) -> Result<RasterBand> {
+    pub fn overview(&self, overview_index: isize) -> Result<RasterBand<'a>> {
         unsafe {
             let c_band = self.c_rasterband;
             let overview = gdal_sys::GDALGetOverview(c_band, overview_index as libc::c_int);
             if overview.is_null() {
                 return Err(_last_null_pointer_err("GDALGetOverview"));
             }
-            Ok(RasterBand::from_c_rasterband(dataset, overview))
+            Ok(RasterBand::from_c_rasterband(self.dataset, overview))
         }
     }
 }
