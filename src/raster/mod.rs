0e00b77fdd67622caddea9d2f4bfd56e88c23bb5
Leonardo Hardtke <leonardo.hardtke@des.qld.gov.au>
2021-06-16 08:58:20 +1000

    move RasterCreationOption to raste/mod.rs

--- src/raster/mod.rs
+++ src/raster/mod.rs
@@ -8,5 +8,11 @@ pub use rasterband::{Buffer, ByteBuffer, ColorInterpretation, RasterBand};
 pub use types::{GDALDataType, GdalType};
 pub use warp::reproject;
 
+#[derive(Debug)]
+pub struct RasterCreationOption<'a> {
+    pub key: &'a str,
+    pub value: &'a str,
+}
+
 #[cfg(test)]
 mod tests;
