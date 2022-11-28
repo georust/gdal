a577a93bf932b1b695f8eec43a9f907af035be53
Simeon H.K. Fitch <fitch@astraea.io>
2022-11-28 13:51:25 -0500

    Reverted bindgen-caused warning suppressions.

--- gdal-sys/src/lib.rs
+++ gdal-sys/src/lib.rs
@@ -3,11 +3,6 @@
 #![allow(non_snake_case)]
 #![allow(clippy::upper_case_acronyms)]
 #![allow(rustdoc::bare_urls)]
-// bindgen test code generates lots of warnings when testing
-// sizes of types due to using constructs such as:
-// `unsafe { &(*(::std::ptr::null::<__sbuf>()))._base as *const _ as usize }`
-// This disables those warnings.
-#![cfg_attr(test, allow(deref_nullptr))]
 
 include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
 include!(concat!(env!("OUT_DIR"), "/docs_rs_helper.rs"));
