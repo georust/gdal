4e263c94e97ca2cf1f2d468766125e9531178012
Johannes Drönner <droenner@informatik.uni-marburg.de>
2023-03-06 12:14:01 +0100

    expect message

--- build.rs
+++ build.rs
@@ -1,7 +1,8 @@
 use std::{env, str::FromStr};
 
 fn main() {
-    let gdal_version_string = env::var("DEP_GDAL_VERSION_NUMBER").unwrap();
+    let gdal_version_string = env::var("DEP_GDAL_VERSION_NUMBER")
+        .expect("The GDAL-SYS crate must emit the version of libgdal via cargo:version_number");
     println!("GDAL version string: \"{gdal_version_string}\"");
 
     // this version string is the result of:
