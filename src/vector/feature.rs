cf3c052cc65aa7de88c92de2eaf5c0664fd70b5a
Laurențiu Nicola <lnicola@dend.ro>
2023-12-21 21:29:01 +0200

    Clean up Feature::set_field_null

--- src/vector/feature.rs
+++ src/vector/feature.rs
@@ -649,17 +649,8 @@ impl<'a> Feature<'a> {
     }
 
     pub fn set_field_null(&self, field_name: &str) -> Result<()> {
-        let c_str_field_name = CString::new(field_name)?;
-        let field_id =
-            unsafe { gdal_sys::OGR_F_GetFieldIndex(self.c_feature(), c_str_field_name.as_ptr()) };
-        if field_id == -1 {
-            return Err(GdalError::InvalidFieldName {
-                field_name: field_name.to_string(),
-                method_name: "OGR_F_GetFieldIndex",
-            });
-        }
-
-        unsafe { gdal_sys::OGR_F_SetFieldNull(self.c_feature(), field_id) };
+        let idx = self.field_idx_from_name(field_name)?;
+        unsafe { gdal_sys::OGR_F_SetFieldNull(self.c_feature(), idx) };
         Ok(())
     }
 
