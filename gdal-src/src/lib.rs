29fea6156874c30127d781e4c070b3d01bd4bf61
Magnus Ulimoen <magnusu@met.no>
2024-10-02 21:30:25 +0200

    Use hdf5-metno fork and netcdf-sys

--- gdal-src/src/lib.rs
+++ gdal-src/src/lib.rs
@@ -3,11 +3,11 @@ extern crate curl_sys;
 #[cfg(feature = "geos")]
 extern crate geos_sys;
 #[cfg(feature = "driver_hdf5")]
-extern crate hdf5_src;
+extern crate hdf5_sys;
 #[cfg(feature = "driver_sqlite")]
 extern crate libsqlite3_sys;
 #[cfg(feature = "driver_netcdf")]
-extern crate netcdf_src;
+extern crate netcdf_sys;
 #[cfg(feature = "driver_pg")]
 extern crate pq_src;
 
