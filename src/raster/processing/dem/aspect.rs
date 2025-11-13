a48c87019c5550f8ac98e5b9585bf83afb09e4e8
Laurențiu Nicola <lnicola@dend.ro>
2025-11-13 16:28:15 +0200

    Increase tolerance in test_aspect

--- src/raster/processing/dem/aspect.rs
+++ src/raster/processing/dem/aspect.rs
@@ -119,7 +119,7 @@ mod tests {
             mean: 165.72752499998,
             std_dev: 98.590199951445,
         };
-        assert_near!(StatisticsAll, stats, expected, epsilon = 1e-5);
+        assert_near!(StatisticsAll, stats, expected, epsilon = 1e-4);
         Ok(())
     }
 }
