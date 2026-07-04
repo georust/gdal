3bddcf35040984a98c5f403574def23042cf404e
Laurențiu Nicola <lnicola@dend.ro>
2026-07-04 13:59:35 +0300

    Allow clippy::missing_safety_doc and clippy::ptr-offset-with-cast

--- gdal-sys/src/lib.rs
+++ gdal-sys/src/lib.rs
@@ -2,6 +2,8 @@
 #![allow(non_camel_case_types)]
 #![allow(non_snake_case)]
 #![allow(unnecessary_transmutes)]
+#![allow(clippy::ptr_offset_with_cast)]
+#![allow(clippy::missing_safety_doc)]
 #![allow(clippy::upper_case_acronyms)]
 #![allow(rustdoc::bare_urls)]
 
